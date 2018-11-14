b421c080db11970569ac0bcec47898d118a7d11b
patlown <lown@wisc.edu>
2018-11-14 10:35:54 -0600

    changed weather,form and titles from classes to const, set up prop passing from app to weather for display, added conditional statements for prop passing

--- src/App.js
+++ src/App.js
@@ -5,24 +5,56 @@ import Weather from "./components/Weather";
 
 const API_KEY = '793b961330b099f727cb548bb38929cb';
 
-class WeatherApp extends React.Component{
-  
-  getWeather = async(e) => {
+
+class WeatherApp extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    desctription: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
 
     const data = await api_call.json();
-    console.log(data);
+    if (city && country && data.main) {
+      console.log(data);
 
-
-    
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        error: "Please enter a valid city and country."
+      });
+    }
   }
   render() {
     return (
       <div>
         <Titles></Titles>
         <Form getWeather={this.getWeather}></Form>
-        <Weather></Weather>
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
+
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-class Weather extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>Weather Component</p>
-            </div>
-        );
-    }
-}
+const Weather = (props) => (
+
+    //use && operator to only render text if props exist, ie, on click of button
+    <div>
+
+        {props.city && props.country && <p>Location: {props.city}, {props.country} </p>}
+
+        {props.temperature && <p>Temperature: {props.temperature}</p>}
+
+        {props.humidity && <p>Humidity: {props.humidity}</p>}
+
+        {props.description && <p>Conditions: {props.description}</p>}
+
+        {props.error && <p>Error: {props.error}</p>}
+
+
+
+    </div>
+
+)
 
 export default Weather;
